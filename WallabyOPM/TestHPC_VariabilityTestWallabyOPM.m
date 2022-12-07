1ee6165af6c887044e5c3eb521800d518612255f
Michael <michael.sternbach@stud.uni-goettingen.de>
2022-12-07 16:35:47 +0100

    added script to test variabilitytest on HPC

---
+++ WallabyOPM/TestHPC_VariabilityTestWallabyOPM.m
@@ -0,0 +1,19 @@
+datafolder = '/scratch1/users/sternbach1/MarsupialData';
+
+disp('load data')
+load([datafolder '/Maps/WallabyH_Image.mat'])
+disp('--------------------------------------------')
+
+disp('make small test data set')
+dimg_small = dimg(1:8,1:10);
+disp('--------------------------------------------')
+
+disp('run main function: VariabilityTestWallabyOPM')
+disp('--------------------------------------------')
+[AllMaps,MaXDeltaOPM,ConfidenceIntervalls] = VariabilityTestWallabyOPM(dimg_small,2,1);
+disp('--------------------------------------------')
+
+disp('save data')
+save([datafolder '/Evaluation/VariabilityTest/TestSmallWallabyH.mat']');
+disp('--------------------------------------------')
+disp('Finished!')
