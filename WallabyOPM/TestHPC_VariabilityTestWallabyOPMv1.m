de8e0d22579a990299b59d43285955be748097c2
Michael <michael.sternbach@stud.uni-goettingen.de>
2022-12-07 16:58:23 +0100

    Changed name

---
+++ WallabyOPM/TestHPC_VariabilityTestWallabyOPMv1.m
@@ -0,0 +1,24 @@
+% module load matlab
+% cd /scratch1/users/sternbach1/MarsupialData/marsupial-data
+% sbatch -c 1 -N 1 -A all -C scratch --mem=16G -t 1:00:00 -o "/scratch1/users/sternbach1/MarsupialData/output/output_TestHPC_VariabilityTestWallabyOPM.txt" --wrap='matlab -batch "TestHPC_VariabilityTestWallabyOPM"'
+
+
+datafolder = '/scratch1/users/sternbach1/MarsupialData';
+
+disp('load data')
+load([datafolder '/Maps/WallabyH_Image.mat'])
+disp('--------------------------------------------')
+
+disp('make small test data set')
+dimg_small = dimg(1:8,1:10);
+disp('--------------------------------------------')
+
+disp('run main function: VariabilityTestWallabyOPM')
+disp('--------------------------------------------')
+[AllMaps,MaXDeltaOPM,ConfidenceIntervalls] = VariabilityTestWallabyOPM(dimg_small,2,1);
+disp('--------------------------------------------')
+
+disp('save data')
+save([datafolder '/Evaluation/VariabilityTest/TestSmallWallabyH.mat']');
+disp('--------------------------------------------')
+disp('Finished!')
