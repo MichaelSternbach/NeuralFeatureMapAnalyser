f055ec478ff0ceac642ffe239bbe98d700219c53
Michael <michael.sternbach@stud.uni-goettingen.de>
2022-12-07 17:25:58 +0100

    add Script path

--- WallabyOPM/TestHPC_VariabilityTestWallabyOPMv1.m
+++ WallabyOPM/TestHPC_VariabilityTestWallabyOPMv1.m
@@ -2,7 +2,7 @@
 % cd /scratch1/users/sternbach1/MarsupialData/marsupial-data/WallabyOPM
 % sbatch -c 1 -N 1 -A all -C scratch --mem=16G -t 1:00:00 -o "/scratch1/users/sternbach1/MarsupialData/output/output_TestHPC_VariabilityTestWallabyOPM.txt" --wrap='matlab -batch "TestHPC_VariabilityTestWallabyOPMv1"'
 
-
+addpath('/scratch1/users/sternbach1/MarsupialData/ScriptJason/')
 datafolder = '/scratch1/users/sternbach1/MarsupialData';
 
 disp('load data')
