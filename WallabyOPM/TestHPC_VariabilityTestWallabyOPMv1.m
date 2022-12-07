5907d903f930f876c83e00ef1c1f712a996b488c
Michael <michael.sternbach@stud.uni-goettingen.de>
2022-12-07 18:26:47 +0100

    change matlab version

--- WallabyOPM/TestHPC_VariabilityTestWallabyOPMv1.m
+++ WallabyOPM/TestHPC_VariabilityTestWallabyOPMv1.m
@@ -1,4 +1,4 @@
-% module load matlab
+% module load matlab/R2020b
 % cd /scratch1/users/sternbach1/MarsupialData/marsupial-data/WallabyOPM
 % sbatch -c 1 -N 1 -A all -C scratch --mem=16G -t 1:00:00 -o "/scratch1/users/sternbach1/MarsupialData/output/output_TestHPC_VariabilityTestWallabyOPM.txt" --wrap='matlab -batch "TestHPC_VariabilityTestWallabyOPMv1"'
 
