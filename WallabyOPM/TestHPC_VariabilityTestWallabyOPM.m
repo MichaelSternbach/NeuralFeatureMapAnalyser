9e734ff17aba0bcfd4ae726e4b97d9546b90b498
Michael <michael.sternbach@stud.uni-goettingen.de>
2022-12-09 15:05:29 +0100

    added cluster bash script

--- WallabyOPM/TestHPC_VariabilityTestWallabyOPM.m
+++ WallabyOPM/TestHPC_VariabilityTestWallabyOPM.m
@@ -1,3 +1,7 @@
+%module load matlab/R2020b
+%cd /scratch1/users/sternbach1/MarsupialData/CompiledCode/TestHPC_VariabilityTestWallabyOPMv1
+%sbatch -c 1 -N 1 -A all -C scratch --mem=16G -t 1:00:00 -o "/scratch1/users/sternbach1/MarsupialData/output/output_TestHPC_VariabilityTestWallabyOPM.txt" --wrap='matlab -batch "!./TestHPC_VariabilityTestWallabyOPMv1"'
+
 datafolder = '/scratch1/users/sternbach1/MarsupialData';
 
 disp('load data')
