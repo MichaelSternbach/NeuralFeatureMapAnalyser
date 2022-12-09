544a243d4d843f0f638d989321e0dd0b7c21b25f
Michael <michael.sternbach@stud.uni-goettingen.de>
2022-12-09 14:20:47 +0100

    removed compiled function

--- WallabyOPM/TestHPC_VariabilityTestWallabyOPMv1/ScriptHPC.m
+++
@@ -1,7 +0,0 @@
-% module load matlab/R2020b
-% cd /scratch1/users/sternbach1/MarsupialData/marsupial-data/WallabyOPM/TestHPC_VariabilityTestWallabyOPMv1
-% sbatch -c 1 -N 1 -A all -C scratch --mem=16G -t 1:00:00 -o "/scratch1/users/sternbach1/MarsupialData/output/output_TestHPC_VariabilityTestWallabyOPM.txt" --wrap='matlab -batch "ScriptHPC"'
-
-
-addpath('/scratch1/users/sternbach1/MarsupialData/ScriptJason/')
-!./TestHPC_VariabilityTestWallabyOPMv1
