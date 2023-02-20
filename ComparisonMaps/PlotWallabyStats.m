8fb9f34da8131c489762892176d5e8c4b0492c99
Michael <michael.sternbach@stud.uni-goettingen.de>
2023-02-20 15:54:21 +0100

    Added Cat data

--- ComparisonMaps/PlotWallabyStats.m
+++ ComparisonMaps/PlotWallabyStats.m
@@ -1,27 +1,33 @@
 FigureFolder = '/home/michael/Cloud/PhD/MarsupialData/marsupial-data/ComparisonMaps/Figures/';
 DataFolder = '/home/michael/Cloud/PhD/MarsupialData/Data/';
 
-%%WallabyH
-%%% All Trials
-BootstrapSampleFile='TestBig700WallabyH.mat';
-JackknifeSampleFile='JackknifeSamplesWallabyH.mat';
-PlotOrientationStatsWallaby(BootstrapSampleFile,JackknifeSampleFile,DataFolder,FigureFolder)
-PlotPWStatsWallaby(BootstrapSampleFile,DataFolder,FigureFolder)
-
-%%% Trial 1-20
-BootstrapSampleFile='BootstrapSamples1000WallabyHTrials1-20.mat';
-JackknifeSampleFile='JackknifeSamplesWallabyHTrials1-20.mat';
-PlotOrientationStatsWallaby(BootstrapSampleFile,JackknifeSampleFile,DataFolder,FigureFolder)
-PlotPWStatsWallaby(BootstrapSampleFile,DataFolder,FigureFolder)
-
-%%% Trial 1-10
-BootstrapSampleFile='BootstrapSamples1000WallabyHTrials1-10.mat';
-JackknifeSampleFile='JackknifeSamplesWallabyHTrials1-10.mat';
-PlotOrientationStatsWallaby(BootstrapSampleFile,JackknifeSampleFile,DataFolder,FigureFolder)
-PlotPWStatsWallaby(BootstrapSampleFile,DataFolder,FigureFolder)
+% %%WallabyH
+% %%% All Trials
+% BootstrapSampleFile='TestBig700WallabyH.mat';
+% JackknifeSampleFile='JackknifeSamplesWallabyH.mat';
+% PlotOrientationStatsWallaby(BootstrapSampleFile,JackknifeSampleFile,DataFolder,FigureFolder)
+% PlotPWStatsWallaby(BootstrapSampleFile,DataFolder,FigureFolder)
+% 
+% %%% Trial 1-20
+% BootstrapSampleFile='BootstrapSamples1000WallabyHTrials1-20.mat';
+% JackknifeSampleFile='JackknifeSamplesWallabyHTrials1-20.mat';
+% PlotOrientationStatsWallaby(BootstrapSampleFile,JackknifeSampleFile,DataFolder,FigureFolder)
+% PlotPWStatsWallaby(BootstrapSampleFile,DataFolder,FigureFolder)
+% 
+% %%% Trial 1-10
+% BootstrapSampleFile='BootstrapSamples1000WallabyHTrials1-10.mat';
+% JackknifeSampleFile='JackknifeSamplesWallabyHTrials1-10.mat';
+% PlotOrientationStatsWallaby(BootstrapSampleFile,JackknifeSampleFile,DataFolder,FigureFolder)
+% PlotPWStatsWallaby(BootstrapSampleFile,DataFolder,FigureFolder)
 
 % %%WallabyC
-% BootstrapSampleFile='TestBig2400WallabyC.mat';
+% BootstrapSampleFile= 'TestBig2400WallabyC.mat';
 % JackknifeSampleFile='JackknifeSamplesWallabyC.mat';
+% PlotOrientationStatsWallaby(BootstrapSampleFile,JackknifeSampleFile,DataFolder,FigureFolder)
+% PlotPWStatsWallaby(BootstrapSampleFile,DataFolder,FigureFolder)
+
+% %%Cat BC
+% BootstrapSampleFile= 'BootstrapSamples1000CatBCTrials1-10.mat';
+% JackknifeSampleFile='JackknifeSamplesCatBCTrials1-10.mat';
 % PlotOrientationStatsWallaby(BootstrapSampleFile,JackknifeSampleFile,DataFolder,FigureFolder)
 % PlotPWStatsWallaby(BootstrapSampleFile,DataFolder,FigureFolder)
