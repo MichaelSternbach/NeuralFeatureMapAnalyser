9be247ba42bf3cdcb22a896dc07b5415a4ac7ac6
Michael <michael.sternbach@stud.uni-goettingen.de>
2023-09-12 09:50:14 +0200

    updated and added functions for plotting

--- OPM_Processing/plot_mapAbs.m
+++ OPM_Processing/plot_mapAbs.m
@@ -1,29 +1,37 @@
-function plot_mapAbs(map,Title,maxMap,minMap,ROI)
-    %figure;
-    if nargin == 4
-        ROI = ones(size(map));
-    elseif nargin == 2
+function plot_mapAbs(map,Title,maxMap,minMap,ROI,NTicks)
+    %% argin
+    if nargin == 2
         maxMap = max(map,[],'all');
         minMap = min(map,[],'all');
     end
+    if nargin <= 4
+        ROI = ones(size(map));
+    end
+    if nargin <= 5
+        NTicks = 8;
+    end
+    
+    %% norm map
     a = (map-minMap)/(maxMap-minMap);
     a=a.*ROI;
+    
+    %% plot map
     imagesc(a); 
     
+    %% details Plot
     %colormap jet;
     colormap turbo;
     %colormap gray;
     %colormap hot;
     
-%     set(gca,'xtick',[])
-%     set(gca,'ytick',[])
-    title(Title, 'interpreter', 'latex')
+    set(gca,'xtick',[])
+    set(gca,'ytick',[])
+    title(Title)%, 'interpreter', 'latex'
     %pbaspect([1 1 1])
     
     hold on
     %colorbar('Ticks',[0,0.5,3])
     cbh = colorbar ; %Create Colorbar
-    NTicks = 8;
 %     axis equal
     axis image
     cbh.Ticks = linspace(0, 1, NTicks) ; %Create 8 ticks from zero to 1
