e97e4aa5eddb7e692db9bc7b66856fab0d9c4492
Michael <michael.sternbach@stud.uni-goettingen.de>
2024-07-01 14:07:49 +0200

    first structure of a function to test modularity of an OPM

---
+++ OPM_Processing/TestModularityOPM.m
@@ -0,0 +1,11 @@
+function TestModularityOPM(data_obj)
+    %% get powerspectrum mean
+    
+    %% determine peak
+    
+    %% determine powerspectrum bootstrap samples
+    
+    %% determine CI spectrum
+    
+    %% determine distribution peak
+end
