09cb6adce8f77c33d4a4b738d5190191dded110d
Michael <michael.sternbach@stud.uni-goettingen.de>
2024-07-04 13:36:41 +0200

    update animal

--- main.m
+++ main.m
@@ -1,13 +1,14 @@
 clear all
 close all
-
+disp('Start OPM analysis')
+disp('-------------------------------------------')
 %% add path to OPM processing functions 
 addpath OPM_Processing/
 
 
 %% animal parameter
-animal = 'ferret';
-specimen_num = 6;
+animal = 'wallaby';
+specimen_num = 1;
 
 %% general parameter data evaluation
 getCI = false;
@@ -42,21 +43,25 @@ position_ColumnSpacing_txt = [10 10];
 x_hypercolumns = 1;
 
 %% load data
+disp('load data')
 [data_info,data_path,data_obj,data,BloodVesselImg] = getAnimalData(animal,specimen_num,AnimalDataFolder);
 data_obj.prepare_samples_array(bootstrapsamples)
-
+disp('-----------------------')
 %% set filter cut offs
+disp('determine filter settings')
 data_info = getFilterSettings(data_obj,data_info,ResultDataFolder,resetFilter,lowpass_cutoffs_mm,profile_range_mm,profile_step_mm);
-
+disp('-----------------------')
 %% calculate column spacing
+disp('calculate column spacing')
 [average_spacing_mm,local_spacing_mm,newROI] =  getColumnsSpacing(data_obj,ResultDataFolder,smallest_w_mm,largest_w_mm,w_step_mm,getCI,FilterMap);
-
+disp('-----------------------')
 %% find pinwheels and calc pinwheel density
+disp('calculate pinwheel positions and pinwheel density')
 PwInfo= getPinwheelInfos(data_obj,local_spacing_mm,ResultDataFolder,newROI,getCI,do_plotting,lowpass_cutoffs_mm,beta_rel);
-
+disp('-----------------------')
 
 %% plot map, pinwheel positions, pinwheel density and column spacing
-
+disp('plot results')
 f = figure();
 
 z = data_obj.filter_map(data_obj.read_map()); %map
@@ -75,6 +80,7 @@ title(['pinwheel density=' num2str(round(PwInfo.WeightedPwDensityFixedFilter,2))
 
 % save figure
 print(f,'-depsc', FigureFileName)
+disp('-----------------------')
 
 
 % %% get covariance data
