c9cc0298254e33369860f65c40c3f5a18d65a1f4
Michael <michael.sternbach@stud.uni-goettingen.de>
2025-02-21 10:44:53 +0100

    add and adjust results plotting functions

--- OPM_Processing/getDirectionData.m
+++ OPM_Processing/getDirectionData.m
@@ -1,4 +1,4 @@
-function [DirectionMap,StimDir] = getDirectionData(data_info,data_path)
+function [DirectionMap,StimDir] = getDirectionData(data_info,data_path,GIF_SN_TH)
     
     SpacingDirectionData = [data_path '/DirectionData_' data_info.animal data_info.ID '.mat'];
     if isfile(SpacingDirectionData)
@@ -8,6 +8,10 @@ function [DirectionMap,StimDir] = getDirectionData(data_info,data_path)
         save(SpacingDirectionData,'DirectionData')
     end
     
+    if GIF_SN_TH >0
+        DirectionData = GIF(DirectionData,GIF_SN_TH);
+    end
+
     StimDir = [data_info.stim_order(find(~isnan(data_info.stim_order))) data_info.stim_order(find(~isnan(data_info.stim_order)))+180];
     StimDir = reshape(StimDir,[1 1 length(StimDir) 1]);
     DirectionMap = mean(DirectionData(:,:,1:size(DirectionData,3)-1,:).*exp(2i*pi*StimDir/360),3:4);
